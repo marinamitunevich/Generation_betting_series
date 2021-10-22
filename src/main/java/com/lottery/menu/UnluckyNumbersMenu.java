fd70efe529bbe846e8219643cf2261f0b36db7a7
marinamitunevich <marina.mitunevich@gmail.com>
2021-10-22 15:17:03 +0200

    implemented showMenu() method in UnluckyNumbersMenu

--- src/main/java/com/lottery/menu/UnluckyNumbersMenu.java
+++ src/main/java/com/lottery/menu/UnluckyNumbersMenu.java
@@ -1,8 +1,59 @@
 package com.lottery.menu;
 
+import com.lottery.ApplicationContext;
+import com.lottery.api.Lottery;
+
+import java.util.Scanner;
+
 public class UnluckyNumbersMenu extends BaseMenu {
+
     @Override
     public void showMenu() {
 
+        Scanner readerFromConsole = ApplicationContext.INSTANCE.getReaderFromConsole();
+
+        System.out.println("Menu unlucky numbers:");
+        System.out.println("1 - add your unlucky numbers");
+        System.out.println("2 - delete your unlucky numbers");
+        System.out.println("3 - back to Menu");
+
+        String readConsole = readerFromConsole.nextLine();
+
+        switch (readConsole) {
+            case "1":
+                addUnluckyNumbers();
+                break;
+            case "2":
+                deleteUnluckyNumbers();
+                break;
+            case "3":
+                ApplicationContext.INSTANCE.getParentMenu().showMenu();
+                break;
+            default:
+                System.out.println("Incorrect data, enter please again: 1 2 or 3");
+                showMenu();
+        }
+    }
+
+    private void deleteUnluckyNumbers() {
+
+        Lottery lottery = null;
+
+        lottery = ApplicationContext.INSTANCE.getLottoLottery();
+        lottery.removeUnluckyNumbers();
+
+        lottery = ApplicationContext.INSTANCE.getEuroJackPotLottery();
+        lottery.removeUnluckyNumbers();
+    }
+
+    private void addUnluckyNumbers() {
+
+        Lottery lottery = null;
+
+        lottery = ApplicationContext.INSTANCE.getLottoLottery();
+        lottery.addUnluckyNumbers();
+
+        lottery = ApplicationContext.INSTANCE.getEuroJackPotLottery();
+        lottery.addUnluckyNumbers();
     }
 }
