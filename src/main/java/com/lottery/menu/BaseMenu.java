f2a1bb11ac42263799d329467a51118a19e9494d
marinamitunevich <marina.mitunevich@gmail.com>
2021-10-22 14:47:27 +0200

    added method exit() to BaseMenu, implemented showMenu() method in MainMenu

--- src/main/java/com/lottery/menu/BaseMenu.java
+++ src/main/java/com/lottery/menu/BaseMenu.java
@@ -1,4 +1,8 @@
 package com.lottery.menu;
 
 public abstract class BaseMenu implements Menu {
+
+    protected void exit() {
+        System.exit(0);
+    }
 }
--- src/main/java/com/lottery/menu/MainMenu.java
+++ src/main/java/com/lottery/menu/MainMenu.java
@@ -1,8 +1,42 @@
 package com.lottery.menu;
 
+import com.lottery.ApplicationContext;
+
+import java.util.Scanner;
+
 public class MainMenu extends BaseMenu{
     @Override
     public void showMenu() {
+
+        Scanner readerFromConsole = ApplicationContext.INSTANCE.getReaderFromConsole();
+
+        System.out.println("Menu:");
+        System.out.println("1 - choose game and generate numbers");
+        System.out.println("2 - unlucky numbers");
+        System.out.println("3 - exit");
+
+        String readConsole = readerFromConsole.nextLine();
+
+        Menu nextMenu = null;
+        switch (readConsole) {
+            case "1":
+                nextMenu = new GameNamesMenu();
+                break;
+            case "2":
+                nextMenu = new UnluckyNumbersMenu();
+                break;
+            case "3":
+                exit();
+                break;
+            default:
+                System.out.println("Incorrect data, enter please again: 1  2 or 3, try again");
+                showMenu();
+        }
+
+        if (nextMenu != null) {
+            nextMenu.showMenu();
+            showMenu();
+        }
 
     }
 }
