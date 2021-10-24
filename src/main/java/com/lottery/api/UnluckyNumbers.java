4b936c6a69e3733ce751fa4f0bec6beccbca497c
marinamitunevich <marina.mitunevich@gmail.com>
2021-10-25 01:27:27 +0200

    added javadoc to methods of Interfaces Lottery UnluckyNumbers Menu

--- src/main/java/com/lottery/api/UnluckyNumbers.java
+++ src/main/java/com/lottery/api/UnluckyNumbers.java
@@ -3,7 +3,32 @@ package com.lottery.api;
 import java.util.List;
 
 public interface UnluckyNumbers {
+
+
+    /**
+     * reads the sting from the file, extracts list of the numbers from string separated by a space
+     * methods sends an log.error
+     * @throws java.io.IOException
+     * @return a list of numbers
+     */
+
     List<Integer> getUnluckyNumbers();
+
+    /**
+     * reads a string of numbers from the console,
+     * extracts numbers from the string, checks for conditions and adds to a file
+     * methods sends an log.error
+     * @throws com.lottery.exceptions.IncorrectRangeOfUnluckyNumber
+     * @throws NumberFormatException
+     */
+
     void addUnluckyNumbers();
+
+    /**
+     * adds an empty line to the file
+     * methods sends an log.error
+     * @throws java.io.IOException
+     */
+
     void removeUnluckyNumbers();
 }
--- src/main/java/com/lottery/menu/Menu.java
+++ src/main/java/com/lottery/menu/Menu.java
@@ -1,5 +1,9 @@
 package com.lottery.menu;
 
 public interface Menu {
+    /**
+     * displays information for the user
+     * methods sends an log.error
+     */
     void showMenu();
 }
